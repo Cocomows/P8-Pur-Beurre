37a5f9702bd1b47d53414859f72cda7ad27f509f
Cocomows <corentin.caradec@gmail.com>
2018-08-23 11:02:54 +0200

    Homepage creation

--- pur_beurre/views.py
+++ pur_beurre/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, "pur_beurre/pages/home.html")
