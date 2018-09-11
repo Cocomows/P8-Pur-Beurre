8abc0d9b834ae92e0ae592677323609d0526caab
Cocomows <corentin.caradec@gmail.com>
2018-09-11 14:57:44 +0200

    first interaction with openfoodfact api

--- pur_beurre/models.py
+++ pur_beurre/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Product(models.Model):
+    NUTRISCORE_CHOICES = (
+        (0, "N/A"),
+        (1, "A"),
+        (2, "B"),
+        (3, "C"),
+        (4, "D"),
+        (5, "E"),
+
+    )
+    name = models.CharField(verbose_name="Nom", max_length=100)
+    brand = models.CharField(verbose_name="Marque", max_length=100)
+    nutriscore = models.SmallIntegerField(choices=NUTRISCORE_CHOICES, default=0)
+    description = models.TextField()
+    image = models.URLField()
+    link_openfoodfacts = models.URLField(verbose_name="URL Openfoodfacts", )
+
+    def __str__(self):
+        return self.name
+
--- pur_beurre/views.py
+++ pur_beurre/views.py
@@ -1,4 +1,5 @@
 
+import requests, json, math
 from django.shortcuts import render
 
 
@@ -7,11 +8,41 @@ def index(request):
 
 
 def results(request):
-    return render(request, "pur_beurre/pages/results.html")
+
+    if request.GET.get('page'):
+        page = int(request.GET.get('page'))
+    else:
+        page = 1
+    page_size = 6
+
+    query = request.GET.get('q')
+
+    api_url = 'https://fr.openfoodfacts.org/cgi/search.pl'
+    payload = {'search_simple': 1,
+               'action': 'process',
+               'json': 1,
+               'page_size': page_size,
+               'search_terms': query,
+               'page': page,
+               # 'sort_by': 'nutrition_grades',
+
+               }
+
+    resp = requests.get(api_url, params=payload)
+    food_json = json.loads(resp.text)
+
+    nb_pages = int(math.ceil(food_json["count"] / int(food_json["page_size"])))
+    api_total_url = resp.url
+    return render(request, "pur_beurre/pages/results.html", locals())
 
 
 def food(request):
-    return render(request, "pur_beurre/pages/food.html")
+    code = request.GET.get('code')
+    api_url = "https://fr.openfoodfacts.org/api/v0/produit/"+str(code)+".json"
+    resp = requests.get(api_url)
+    food_json = json.loads(resp.text)
+
+    return render(request, "pur_beurre/pages/food.html", locals())
 
 
 def legal(request):
